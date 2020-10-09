eeb2c4b549c26601abd72ab1c09af4f8f1875ea0
steen.pihl.kristensen <steen.pihl.kristensen@ftz.dk>
2020-10-09 07:14:09 +0200

    Peger nu på SpkvinApi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://spkvinoapi.azurewebsites.net',
+  apiUrl: 'https://spkvinapi.azurewebsites.net',
   // apiUrl: 'https://spkwineapi.azurewebsites.net', gammle azure subscription
   baseTitle: 'VCA',
   baseLongTitle: 'Wine cellar Api'
